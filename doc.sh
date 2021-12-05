b8fb1e1130b10b4ee49677f95352c0401aeb0415
Thomas Depierre <depierre.thomas@gmail.com>
2021-12-05 17:11:06 +0100

    add documentation to the root module

---
+++ doc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Setup:
+#
+#     mix escript.install github elixir-lang/ex_doc
+#     asdf install erlang 24.0.2
+#     asdf local erlang 24.0.2
+
+rebar3 compile
+rebar3 as docs edoc
+version=0.1.1
+ex_doc "ken" $version "_build/default/lib/ken/ebin" \
+  --source-ref v${version} \
+  --config docs.config "$@"
--- src/ken.erl
+++ src/ken.erl
@@ -1,6 +1,22 @@
+%%%-------------------------------------------------------------------
+%% @doc `ken' allows you to get the shortest round-trip representation of a binary encoding an IEEE 754 floating point number.
+%%
+%% Ken use the Ryu algorithm from Ulf Adams in order to get a fast and precise output.
+%% It has been modified to keep the erlang/OTP representation of floating point numbers as string.
+%%
+%% @end
+%%
+%% @reference See <a href="https://github.com/ulfjack/ryu">https://github.com/ulfjack/ryu</a>
+%%
+%% @end
+%%%-------------------------------------------------------------------
+
 -module(ken).
 
 -export([binary_to_iodata/1]).
 
+%% @doc Take the binary of a 64 bits IEEE 754 floating point number and return
+%% an Iodata representing the shortest round trip decimal string representation
+%% of it by the erlang rules for such.
 binary_to_iodata(<<_:64>> = Binary) ->
     ken_ryu_f64:fwrite_g(Binary).
